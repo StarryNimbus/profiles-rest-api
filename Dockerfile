bab33959353ec6221deb45c7a6c0430416d6248a
Starry Nimbus <starrynimbus01000@gmail.com>
2025-01-16 23:52:11 -0800

    Add bash script and requirements.txt to install dependencies for project

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python
+
+WORKDIR /usr/src/app
+
+RUN apt-get update && apt-get -y install vim
+RUN python -m venv ~/env
+
+# COPY . .
+
+# Still need to manually run this script due to error
+# '/bin/sh: 1: source: not found'
+RUN chmod +x startVirtualEnvironment.sh
+
+# CMD [ "python3", "./helloWorld.py" ]
+CMD ["tail", "-f", "/dev/null"]
---
+++ startVirtualEnvironment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# to run source ./startVirtualEnvironment.sh
+
+# Activate the virtual environment
+source ~/env/bin/activate
+
+# Optional: Print a message to confirm activation
+echo "Virtual environment activated. Python version: $(python --version)"
+
+pip install -r /usr/src/app/requirements.txt
+
+# Create a new Django project and app
+django-admin startproject profiles_project .
+python manage.py startapp profiles_api
