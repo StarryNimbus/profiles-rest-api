28d90a9d77b9dbc4bb22538d7b1c97a6b569ff40
Starry Nimbus <starrynimbus01000@gmail.com>
2025-01-20 14:07:59 -0800

    Updated admin.py and fixed models.py

--- profiles/profiles_api/models.py
+++ profiles/profiles_api/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+
 class UserProfileManager(BaseUserManager):
     """
-    Manager for UserProfiles
+    Manager for user profiles
     """
 
     def create_user(self, email, name, password=None):
@@ -13,29 +14,30 @@ class UserProfileManager(BaseUserManager):
         Create a new user profile
         """
         if not email:
-            raise ValueError('User must have an email address')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
-        # django can encrypt the password using set_password
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_super_user(self, email, name, password=None):
+    def create_superuser(self, email, name, password):
         """
         Create and save a new superuser with given details
         """
         user = self.create_user(email, name, password)
+
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
         return user
 
-class UserProfile (AbstractBaseUser, PermissionsMixin):
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """
     Database model for users in the system
     """
@@ -47,11 +49,11 @@ class UserProfile (AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """
-        Retrieve full name of user
+        Retrieve full name for user
         """
         return self.name
 
@@ -63,6 +65,6 @@ class UserProfile (AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         """
-        Return string representation of our user
+        Return string representation of user
         """
         return self.email
